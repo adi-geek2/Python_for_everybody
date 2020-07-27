6526ea546f4fac23872e8d41406cfd244b1e84d0
Aditya Deshpande <aditya.deshpande@fdtech.de>
2020-07-27 15:05:23 +0200

    added_assignment_8_4

--- list_and_strings.py
+++ list_and_strings.py
@@ -1,1 +1,12 @@
-print("Hello world")
+fname = input("Enter file name: ")
+fh = open(fname)
+words = list()
+word_list = list()
+for line in fh:
+    line.rstrip()
+    words = line.split()
+    for word in words:
+        if word in word_list : continue
+        word_list.append(word)
+word_list.sort()
+print(word_list) 
